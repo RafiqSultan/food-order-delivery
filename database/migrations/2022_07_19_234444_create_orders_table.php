a4a8841e6369390d2221265ebb81cfde650b1b25
RafiqSultan <Rafiqaasultan@gmail.com>
2022-07-22 07:49:24 -0700

    FIX migrations files

--- database/migrations/2022_07_19_234444_create_orders_table.php
+++ database/migrations/2022_07_19_234444_create_orders_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
-            $table->timestamp('dateTime')->nullable();
+            $table->timestamp('dateTime');
             $table->boolean('completed')->default(false);
             $table->string('type'); // dineIn, takeAway
             $table->timestamps();
