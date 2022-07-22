08e05519f030c0e946cc9fb6ffd8add74f6c396f
RafiqSultan <Rafiqaasultan@gmail.com>
2022-07-22 09:23:29 -0700

    FIX migrations files

--- database/migrations/2022_07_22_140844_create_cart_items_table.php
+++ database/migrations/2022_07_22_140844_create_cart_items_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('menu_id');
-            $table->unsignedBigInteger('order_id')->nullable();
+            $table->unsignedBigInteger('order_id')->nullable()->constrained();
             $table->foreign('user_id')->references('id')->on('users');
             $table->foreign('menu_id')->references('id')->on('menus');
             $table->foreign('order_id')->references('id')->on('orders');
